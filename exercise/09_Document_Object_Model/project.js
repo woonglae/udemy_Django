c89b36daafeaa2fcc77b984574f474a641b32372
Woongrae Cho <woonglae@gmail.com>
2019-07-31 17:28:07 -0400

    complete the project of event

---
+++ exercise/09_Document_Object_Model/project.js
@@ -0,0 +1,24 @@
+
+var squares = document.querySelectorAll('td');
+
+// function changeMarker() {
+//   if (this.textContent === '') {
+//     this.textContent = 'X';
+//   } else if (this.textContent === 'X') {
+//     this.textContent = 'O';
+//   } else {
+//     this.textContent = '';
+//   }
+// }
+
+for (var i = 0; i < squares.length; i++) {
+  squares[i].addEventListener('click', function() {
+    if (this.textContent === '') {
+      this.textContent = 'X';
+    } else if (this.textContent === 'X') {
+      this.textContent = 'O';
+    } else {
+      this.textContent = '';
+    }
+  })
+}
