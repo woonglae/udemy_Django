a821c8585e07e9d7c251aa7b472fcb69dad64b57
Woongrae Cho <woonglae@gmail.com>
2019-08-02 13:27:00 -0400

    complete exercise

--- exercise/11_Python_1/Exercise.py
+++ exercise/11_Python_1/Exercise.py
@@ -1,31 +1,31 @@
-#####################################
-#### PART 9: FUNCTION EXERCISES #####
-#####################################
+# #####################################
+# #### PART 9: FUNCTION EXERCISES #####
+# #####################################
 
 
-# Complete the tasks below by writing functions! Keep in mind, these can be
-# really tough, its all about breaking the problem down into smaller, logical
-# steps. If you get stuck, don't feel bad about having to peek to the solutions!
+# # Complete the tasks below by writing functions! Keep in mind, these can be
+# # really tough, its all about breaking the problem down into smaller, logical
+# # steps. If you get stuck, don't feel bad about having to peek to the solutions!
 
-#####################
-## -- PROBLEM 1 -- ##
-#####################
+# #####################
+# ## -- PROBLEM 1 -- ##
+# #####################
 
-# Given a list of integers, return True if the sequence of numbers 1, 2, 3
-# appears in the list somewhere.
+# # Given a list of integers, return True if the sequence of numbers 1, 2, 3
+# # appears in the list somewhere.
 
-# For example:
+# # For example:
 
 def arrayCheck(nums):
     for i in range(len(nums)-2):
-        if nums[i] == 1 and nums[i+1] == 2 and nums[i+2] == 3 :
-            print(nums[i], nums[i+1], nums[i+2] + "\n")
-            return True
-        return False
+        if nums[i]==1:
+            if nums[i+1]==2:
+                if nums[i+2]==3:
+                    return True
+    return False
 print(arrayCheck([1, 1, 2, 3, 1]) )
 print(arrayCheck([1, 1, 2, 4, 1]) )
 print(arrayCheck([1, 1, 2, 1, 2, 3]) )
-
 
 # #####################
 # ## -- PROBLEM 2 -- ##
@@ -40,87 +40,116 @@ print(arrayCheck([1, 1, 2, 1, 2, 3]) )
 # # stringBits('Hi') → 'H'
 # # stringBits('Heeololeo') → 'Hello'
 
-# def stringBits(str):
-  
+def stringBits(str):
+    result = ""
+    for i in range(len(str)):
+        if i%2 == 0:
+            result += str[i]
+    return result
 
+print(stringBits('Hello'))
+print(stringBits('Hi') )
+print(stringBits('Heeololeo') )
 
-# #####################
-# ## -- PROBLEM 3 -- ##
-# #####################
+#####################
+## -- PROBLEM 3 -- ##
+#####################
 
-# # Given two strings, return True if either of the strings appears at the very end
-# # of the other string, ignoring upper/lower case differences (in other words, the
-# # computation should not be "case sensitive").
-# #
-# # Note: s.lower() returns the lowercase version of a string.
-# #
-# # Examples:
-# #
-# # end_other('Hiabc', 'abc') → True
+# Given two strings, return True if either of the strings appears at the very end
+# of the other string, ignoring upper/lower case differences (in other words, the
+# computation should not be "case sensitive").
+#
+# Note: s.lower() returns the lowercase version of a string.
+#
+# Examples:
+#
+# end_other('Hiabc', 'abc') → True
 # # end_other('AbC', 'HiaBc') → True
 # # end_other('abc', 'abXabc') → True
 
 
-# def end_other(a, b):
-#   if (a[-1]==b[-1]) :
-#       return true
+def end_other(a, b):
+    a = a.lower()
+    b = b.lower()
+    return ( a[-(len(b)):] == b or a == b[-(len(a)):])
+    
+print(end_other('Hiabc', 'abc'))
+print(end_other('AbC', 'HiaBc'))
+print(end_other('abc', 'abXabc'))
 
 # #####################
-# ## -- PROBLEM 4 -- ##
-# #####################
+## -- PROBLEM 4 -- ##
+#####################
 
-# # Given a string, return a string where for every char in the original,
-# # there are two chars.
+# Given a string, return a string where for every char in the original,
+# there are two chars.
 
 # # doubleChar('The') → 'TThhee'
 # # doubleChar('AAbb') → 'AAAAbbbb'
 # # doubleChar('Hi-There') → 'HHii--TThheerree'
 
-# def doubleChar(str):
-#   result = []
-#   for ch in str: 
-#       result.append(ch)
-#       result.append(ch)
+def doubleChar(str):
+    result = ""
+    for ch in str: 
+      result += ch * 2
+    return result
+
+print(doubleChar('The'))
+print(doubleChar('AAbb') )
+print(doubleChar('Hi-There') )
 
 
+#####################
+## -- PROBLEM 5 -- ##
+#####################
 
-# #####################
-# ## -- PROBLEM 5 -- ##
-# #####################
+# Read this problem statement carefully!
 
-# # Read this problem statement carefully!
+# Given 3 int values, a b c, return their sum. However, if any of the values is a
+# teen -- in the range 13-19 inclusive -- then that value counts as 0, except 15
+# and 16 do not count as a teens. Write a separate helper "def fix_teen(n):"that
+# takes in an int value and returns that value fixed for the teen rule.
+#
+# In this way, you avoid repeating the teen code 3 times (i.e. "decomposition").
+# Define the helper below and at the same indent level as the main no_teen_sum().
+# Again, you will have two functions for this problem!
+#
+# Examples:
+#
+# no_teen_sum(1, 2, 3) → 6
+# no_teen_sum(2, 13, 1) → 3
+# no_teen_sum(2, 1, 14) → 3
 
-# # Given 3 int values, a b c, return their sum. However, if any of the values is a
-# # teen -- in the range 13-19 inclusive -- then that value counts as 0, except 15
-# # and 16 do not count as a teens. Write a separate helper "def fix_teen(n):"that
-# # takes in an int value and returns that value fixed for the teen rule.
-# #
-# # In this way, you avoid repeating the teen code 3 times (i.e. "decomposition").
-# # Define the helper below and at the same indent level as the main no_teen_sum().
-# # Again, you will have two functions for this problem!
-# #
-# # Examples:
-# #
-# # no_teen_sum(1, 2, 3) → 6
-# # no_teen_sum(2, 13, 1) → 3
-# # no_teen_sum(2, 1, 14) → 3
+def no_teen_sum(a, b, c):
+    return fix_teen(a) + fix_teen(b) + fix_teen(c)
+def fix_teen(n):
+    if n in [13,14,17,18,19]:
+        return 0
+    return n
 
-# def no_teen_sum(a, b, c):
-#   # CODE GOES HERE
-# def fix_teen(n):
-#   # CODE GOES HERE
+print(no_teen_sum(1, 2, 3))
+print(no_teen_sum(2, 13, 1))
+print(no_teen_sum(2, 1, 14))
 
-# #####################
-# ## -- PROBLEM 6 -- ##
-# #####################
+#####################
+## -- PROBLEM 6 -- ##
+#####################
 
-# # Return the number of even integers in the given array.
-# #
-# # Examples:
-# #
-# # count_evens([2, 1, 2, 3, 4]) → 3
-# # count_evens([2, 2, 0]) → 3
-# # count_evens([1, 3, 5]) → 0
+# Return the number of even integers in the given array.
+#
+# Examples:
+#
+# count_evens([2, 1, 2, 3, 4]) → 3
+# count_evens([2, 2, 0]) → 3
+# count_evens([1, 3, 5]) → 0
 
-# def count_evens(nums):
-#   # CODE GOES HERE
+def count_evens(nums):
+    result = 0
+    for i in nums:
+        if i % 2 == 0:
+            result += 1
+    return result
+
+print(count_evens([2, 1, 2, 3, 4]))
+print(count_evens([2, 2, 0]))
+print(count_evens([1, 3, 5]))
