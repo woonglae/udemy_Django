ad30af5b7c9afcf00ca14420fa4e19ac68fa49cc
Woongrae Cho <woonglae@gmail.com>
2019-08-08 15:01:07 -0400

    Use Faker, makes fake db

--- first_project/populate_first_app.py
+++ first_project/populate_first_app.py
@@ -16,3 +16,23 @@ def add_topic():
     t = Topic.objects.get_or_create(top_name = random.choice(topics))[0]
     t.save()
     return t
+
+def populate(N=5):
+
+    for entry in range(N):
+        # get the topic for the entry
+        top = add_topic()
+
+        fake_url = fakegen.url()
+        fake_date = fakegen.date()
+        fake_name = fakegen.company()
+
+        webpg = Webpage.objects.get_or_create(topic=top, url=fake_url, name=fake_name)[0]
+
+        acc_rec = AccessRecord.objects.get_or_create(name=webpg, date=fake_date)[0]
+
+
+if __name__=='__main__':
+    print("populateing script!")
+    populate(20)
+    print("populating complete!")
