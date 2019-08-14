172cb2e6e0607b2d6faaefd683b7114f3246e086
Woongrae Cho <woonglae@gmail.com>
2019-08-13 23:38:08 -0400

    Fix typo

--- basicforms/basicapp/forms.py
+++ basicforms/basicapp/forms.py
@@ -3,4 +3,4 @@ from django import forms
 class FormName(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
-    tex = forms.CharField(widget=forms.Textarea)
+    text = forms.CharField(widget=forms.Textarea)
