dc58ac6eec668935797a82604db3d690d08df0b4
Woongrae Cho <woonglae@gmail.com>
2019-08-14 00:05:24 -0400

    See the POST data in console

--- basicforms/basicapp/views.py
+++ basicforms/basicapp/views.py
@@ -8,4 +8,15 @@ def index(request):
 
 def form_name_view(request):
     form = forms.FormName()
+
+    if request.method == 'POST':
+
+        form = forms.FormName(request.POST)
+
+        if form.is_valid():
+            print('VALIDATION SUCCESS!')
+            print("NAME: "+form.cleaned_data['name'])
+            print("EMAIL: "+form.cleaned_data['email'])
+            print("TEXT: "+form.cleaned_data['text'])
+
     return render(request,'basicapp/form_page.html', {'form':form})
