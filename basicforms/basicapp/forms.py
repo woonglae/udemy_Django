2268e7e11e3146e689ba4ad5a284c998ac22f113
Woongrae Cho <woonglae@gmail.com>
2019-08-14 18:00:24 -0400

    Using built-in validator

--- basicforms/basicapp/forms.py
+++ basicforms/basicapp/forms.py
@@ -1,11 +1,13 @@
 from django import forms
+from django.core import validators
 
 class FormName(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
     botcatcher = forms.CharField(required=False,
-                                widget=forms.HiddenInput)
+                                widget=forms.HiddenInput,
+                                validators=[validators.MaxLengthValidator(0)])
 
 
     def clean_botcatcher(self):
